f954022151cfbefdfdf4b7b15ca314d2665519a7
Darren Kulp <darren@kulp.ch>
2020-05-09 09:49:20 -0700

    Use native Cargo functionality to trigger bindgen
    `build.rs` was attempting to avoid re-running `bindgen` by checking
    modification times, but this prevented rerunning `bindgen` under some
    circumstances when `build.rs` itself was changed (for example, to
    introduce `bindgen` symbol whitelisting). Use Cargo's built-in
    functionality to determine whether a rebuild should be done, both using
    
        println!("cargo:rerun-if-changed={}", path.display());
    
    and using `bindgen::CargoCallbacks`, which necessitates updating
    `bindgen` to version 0.53.

--- build.rs
+++ build.rs
@@ -3,8 +3,6 @@ extern crate bindgen;
 use std::env;
 use std::path::{PathBuf, Path};
 use std::process::Command;
-use std::fs;
-use std::io::Result;
 
 fn build_lightning(prefix: &str) {
     Command::new("./build-lightning.sh")
@@ -24,24 +22,6 @@ fn lightning_built(prefix: &Path) -> bool {
     prefix.exists()
 }
 
-fn _need_bindings_built_res(prefix: &PathBuf) -> Result<bool> {
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    let mt1 = fs::metadata(prefix.join("include").join("lightning.h").to_str().unwrap())?.modified()?;
-    let mt2 = fs::metadata("C/lightning-sys.h")?.modified()?;
-
-    let targett = fs::metadata(out_path.join("bindings.rs").to_str().unwrap())?.modified()?;
-
-    Ok(targett < mt1 || targett < mt2)
-}
-
-fn need_bindings_built(prefix: &PathBuf) -> bool {
-    match _need_bindings_built_res(prefix) {
-        Ok(b) => b,
-        Err(_) => true,
-    }
-}
-
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let prefix = out_path.join("lightning-prefix");
@@ -58,17 +38,21 @@ fn main() {
     println!("cargo:rustc-link-lib=static=lightning");
     println!("cargo:rustc-link-lib=static=lightningsys");
 
-    if need_bindings_built(&prefix) {
-        let bindings = bindgen::Builder::default()
-            .header("wrapper.h")
-            .rustfmt_bindings(true)
-            .clang_arg(format!("-I{}", incdir.to_str().unwrap()))
-            .generate()
-            .expect("Unable to generate bindings");
+    // Tell cargo to rerun the build if these files changed.
+    println!("cargo:rerun-if-changed=C/lightning-sys.h");
 
-        // Write the bindings to the $OUT_DIR/bindings.rs file.
-        bindings
-            .write_to_file(out_path.join("bindings.rs"))
-            .expect("Couldn't write bindings!");
-    }
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        // Tell bindgen to regenerate bindings if the wrapper.h's contents or transitively
+        // included files change.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .rustfmt_bindings(true)
+        .clang_arg(format!("-I{}", incdir.to_str().unwrap()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
