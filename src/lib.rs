218d973044d7d551b8f31adb129ca17ce2ab15b8
Darren Kulp <darren@kulp.ch>
2020-05-11 06:02:42 -0700

    Un-suppress some lints no longer triggered

--- src/lib.rs
+++ src/lib.rs
@@ -167,9 +167,7 @@
 // Suppress some lints for bindings specifically.
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
 #[allow(dead_code)]
-#[allow(improper_ctypes)] // TODO
 #[allow(clippy::unreadable_literal)]
 #[allow(clippy::trivially_copy_pass_by_ref)]
 #[allow(clippy::useless_transmute)]
